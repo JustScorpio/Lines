8f06589fbcb24d94458cd715f24aa4b3cfbcf806
Kosarev <skorpiello@gmail.com>
2022-04-23 14:49:36 +0500

    Добавьте файлы проекта.

---
+++ Lines/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Lines
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonHoriz = new System.Windows.Forms.Button();
+            this.buttonVert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonHoriz
+            // 
+            this.buttonHoriz.Location = new System.Drawing.Point(12, 415);
+            this.buttonHoriz.Name = "buttonHoriz";
+            this.buttonHoriz.Size = new System.Drawing.Size(223, 23);
+            this.buttonHoriz.TabIndex = 0;
+            this.buttonHoriz.Text = "Рисовать горизонтально";
+            this.buttonHoriz.UseVisualStyleBackColor = true;
+            this.buttonHoriz.Click += new System.EventHandler(this.buttonHoriz_Click);
+            // 
+            // buttonVert
+            // 
+            this.buttonVert.Location = new System.Drawing.Point(565, 415);
+            this.buttonVert.Name = "buttonVert";
+            this.buttonVert.Size = new System.Drawing.Size(223, 23);
+            this.buttonVert.TabIndex = 1;
+            this.buttonVert.Text = "Рисовать вертикально";
+            this.buttonVert.UseVisualStyleBackColor = true;
+            this.buttonVert.Click += new System.EventHandler(this.buttonVert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonVert);
+            this.Controls.Add(this.buttonHoriz);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonHoriz;
+        private System.Windows.Forms.Button buttonVert;
+    }
+}
+
---
+++ Lines/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lines
+{
+    public partial class Form1 : Form
+    {
+
+        int formWidth;
+        int formHeight;
+
+        Random rnd;
+        Graphics g;
+
+        Color[] colors;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            formWidth = Width;
+            formHeight = Height;
+
+            rnd = new Random();
+            g = this.CreateGraphics();
+            colors = new Color[] 
+            {
+                Color.Black, 
+                Color.Red, 
+                Color.Green, 
+                Color.Yellow, 
+                Color.Blue, 
+                Color.Violet, 
+                Color.Aquamarine,
+                Color.Azure,
+                Color.Brown,
+                Color.DeepSkyBlue,
+                Color.AliceBlue,
+                Color.LightPink,
+                Color.IndianRed,
+                Color.GreenYellow
+            };
+        }
+
+        private void buttonHoriz_Click(object sender, EventArgs e)
+        {
+            var varWidth = rnd.Next(1, 5);
+            var varColor = colors[rnd.Next(0, colors.Length)];
+            var xStart = rnd.Next(5, formWidth - 5);
+            var yStart = rnd.Next(5, formHeight - 5);
+            var xEnd = rnd.Next(5, formWidth - 5);
+
+            g.DrawLine(new Pen(varColor, varWidth), xStart, yStart, xEnd, yStart);
+        }
+
+        private void buttonVert_Click(object sender, EventArgs e)
+        {
+            var varWidth = rnd.Next(1, 5);
+            var varColor = colors[rnd.Next(0, colors.Length)];
+            var xStart = rnd.Next(5, formWidth - 5);
+            var yStart = rnd.Next(5, formHeight - 5);
+            var yEnd = rnd.Next(5, formHeight - 5);
+
+            g.DrawLine(new Pen(varColor, varWidth), xStart, yStart, xStart, yEnd);
+        }
+    }
+}
